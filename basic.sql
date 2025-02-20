7986370d5c612b7396cb3e3605e36ee924585588
Alina Korn <ak1.0@yandex.ru>
2025-02-20 17:32:49 +0300

    Update basic.sql

--- basic.sql
+++ basic.sql
@@ -1,4 +1,16 @@
+Это выберет 2 самых старых клиентов.
+Мы использовали предложение WHERE для фильтрации строк, 
+для которых не было определено значение возраста.
+
 SELECT * FROM Customers
 WHERE age IS NOT NULL
 ORDER BY age DESC
 LIMIT 2
+
+Чтобы выбрать строки с 3-й по 5-ю из таблицы Employees, 
+отсортированные по столбцу salary в порядке убывания:
+
+SELECT * 
+FROM employees
+ORDER BY salary DESC
+LIMIT 3 OFFSET 2
